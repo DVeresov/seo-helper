c70cf9148fecda41cffc8e97ff353c3ae1cc9e7f
weasleyt <veresovdanil@gmail.com>
2026-04-04 16:50:06 +0300

    feat: delete call error in handler method in Kernel class

--- framework/src/Http/Kernel.php
+++ framework/src/Http/Kernel.php
@@ -25,8 +25,6 @@ class Kernel
     {
         try {
 
-            throw new \Exception("Some error");
-
             [$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
 
             $response = call_user_func_array($routeHandler, $vars);
